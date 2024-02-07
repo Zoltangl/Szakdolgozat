eea8961623aed3bfe92af90675f2f9c26ceaa5eb
Zoltangl <zoltangulcsik@gmail.com>
2024-02-07 18:10:34 +0100

    próba
    alapok kezdése

---
+++ signup/connection.php
@@ -0,0 +1,29 @@
+<?php
+
+class DataBase
+{
+
+  private $servername = "localhost";
+  private $username = "admin";
+  private $password = "-Y3NUceruQG!YTAf";
+  private $db = "Szakdolgozat";
+  public static $conn;
+
+  function __construct()
+  {
+    // Create connection
+    self::$conn = new mysqli($this->servername, $this->username, $this->password, $this->db);
+
+    // Check connection
+    if (self::$conn->connect_error) {
+      die("Connection failed: " . $this->conn->connect_error);
+    }
+
+    self::$conn->set_charset("utf8");
+  }
+
+}
+
+
+
+?>
---
+++ signup/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width-device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FU">
+</head>
+<body>
+    <div id="form">
+        <h1 id="heading">Regisztráció<h1>
+            <form name= "form">
+                <input type ="text" id="user" name="user" placeholder="Add meg a Vezetékneved..." required>break break
+                <input type ="text" id="user" name="user" placeholder="Add meg a Keresztneved..." required>break break
+                <input type ="email" id="email" name="email" placeholder="Add meg az email címed..." required>break break
+                <input type ="password" id="pass" name="pass" placeholder="Add meg a jelszavad..." required>break break
+                <input type ="password" id="cpass" name="cpass" placeholder="Jelszó megerősítése..." required>break break
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3">
+</body>
+</html> 
