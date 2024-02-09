7d2c6ae8cc553c16ed94eb91e2d82d964b401cc6
Zoltangl <zoltangulcsik@gmail.com>
2024-02-09 13:03:22 +0100

    update
    frissitese a menunek

--- menu/menu.php
+++ menu/menu.php
@@ -1,73 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Szállásfoglalás</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+z24KPTw8jrlX/1fGTj5n2K4JoqO5d8WQVrNhZ6" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css"> 
+    <title>Navigációs sáv</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FU">
+    <link  href="/Szakdolgozat/css/menu.css">
+    
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">Szállásfoglalás</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Főoldal</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Szállások</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Kapcsolat</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Bejelentkezés</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-
-    <main>
-        <section class="hero-section">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <h1>Üdvözöljük a Szállásfoglaláson!</h1>
-                        <p>
-                            Fedezze fel és foglalja le álmai szállását könnyedén és gyorsan.
-                        </p>
-                        <a href="#" class="btn btn-primary">Szállások megtekintése</a>
-                    </div>
-                    <div class="col-lg-6">
-                        <img src="images/hotel-booking.svg" alt="Hotel Booking" class="img-fluid">
-                    </div>
-                </div>
-            </div>
-        </section>
-    </main>
-
-    <footer class="bg-dark text-light text-center py-4">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <p>&copy; 2023 Szállásfoglalás</p>
+            <a class="navbar-brand" href="#">Oldal neve</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Menü
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Kezdőlap</a></li>
+                            <li><a class="dropdown-item" href="#">Szállás</a></li>
+                            <li><a class="dropdown-item" href="#">Kapcsolat</a></li>
+                            <li><a class="dropdown-item" href="#">Regisztráció/Bejelentkezés</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </footer>
+    </nav>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-uvFvp41aTJvveZsdaR4I8pzhD25msa/T7MDmPwPqZaZwXKWE4w/YLnwFVwlT/GeQ"
-        crossorigin="anonymous"></script>
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-9ndCyUaIbzAi2FU"></script>
 </body>
 
 </html>
