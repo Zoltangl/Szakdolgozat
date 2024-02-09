8cbad9bdd749d72cb3ccd7b3042ce2c3f5928778
Zoltangl <zoltangulcsik@gmail.com>
2024-02-09 11:30:42 +0100

    delete
    továbbra sem engedi behívni

--- js/kartyak.js
+++
@@ -1,8 +0,0 @@
-// kartyak.js
-
-// Belépés linkre kattintva megjeleníti a Bejelentkezés kártyát és elrejti a Regisztrációs kártyát
-document.getElementById("showLoginForm").addEventListener("click", function(event) {
-    event.preventDefault();
-    document.getElementById("form").style.display = "none";
-    document.getElementById("loginForm").style.display = "block";
-});
