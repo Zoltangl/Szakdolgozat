039e5ad362ab241d569e7693fde669b63247f1ad
Zoltangl <zoltangulcsik@gmail.com>
2024-02-15 13:11:14 +0100

    linkek létrehozása
    terms and cond...,  privacy és policy ablak létrehozása és feltöltése

---
+++ view/privacypolicy.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="utf-8">
+    <title>HappyHotel</title>
+    <link rel="icon" href="../img/logo.jpg" type="image/jpg">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-xxl bg-white p-0">
+
+        <!-- Header Start -->
+        <div class="container-fluid bg-dark px-0">
+            <div class="row gx-0">
+                <div class="col-lg-3 bg-dark d-none d-lg-block">
+                    <a href="index.php" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                        <h1 class="m-0 text-primary text-uppercase">HappyHotel</h1>
+                    </a>
+                </div>
+                <div class="col-lg-9">
+                    <div class="row gx-0 bg-white d-none d-lg-flex">
+                        <div class="col-lg-7 px-5 text-start">
+                            <div class="h-100 d-inline-flex align-items-center py-2 me-4">
+                                <i class="fa fa-envelope text-primary me-2"></i>
+                                <p class="mb-0">info@example.com</p>
+                            </div>
+                            <div class="h-100 d-inline-flex align-items-center py-2">
+                                <i class="fa fa-phone-alt text-primary me-2"></i>
+                                <p class="mb-0">+012 345 6789</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 px-5 text-end">
+                            <div class="d-inline-flex align-items-center py-2">
+                                <a class="me-3" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                <a class="me-3" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                                <a class="me-3" href="https://hu.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                                <a class="me-3" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
+                        <a href="index.php" class="navbar-brand d-block d-lg-none">
+                            <h1 class="m-0 text-primary text-uppercase">HappyHotel</h1>
+                        </a>
+                        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                            <div class="navbar-nav mr-auto py-0">
+                                <a href="index.php" class="nav-item nav-link active">Home</a>
+                                <a href="about.php" class="nav-item nav-link">About</a>
+                                <a href="service.php" class="nav-item nav-link">Services</a>
+                                <a href="room.php" class="nav-item nav-link">Rooms</a>
+                                <a href="kedvezmenyeink.php" class="nav-item nav-link">Kedvezményeink</a>
+
+                                <a href="booking.php" class="nav-item nav-link">Booking</a>
+                                <a href="signup.php" class="nav-item nav-link">Registration/Login</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+
+<body>
+    <header>
+        <h1>Adatvédelmi irányelvek</h1>
+    </header>
+    <main>
+        <section id="adatok">
+            <h2>Milyen adatokat gyűjtünk</h2>
+            <p>Az oldalunkon látogatóinkról anonim, összesített adatokat gyűjtünk, mint például a látogatók száma és az oldalak megtekintési statisztikái. 
+A szálloda weboldala látogatása során automatikusan gyűjtött adatok közé tartoznak a látogatók IP-címe, a böngésző típusa, az oldalak megtekintésének időpontja és az általuk használt eszközök információi, amelyeket a webanalitikai eszközök segítségével rögzítünk az oldal teljesítményének elemzése és fejlesztése céljából.</p>
+        </section>
+        <section id="hasznalat">
+            <h2>Adatok felhasználása</h2>
+            <p>A gyűjtött adatokat csak az oldalunk fejlesztésére és javítására használjuk fel. Ezeket az adatokat harmadik felekkel nem osztjuk meg.</p>
+        </section>
+        <section id="vedelem">
+            <h2>Adatvédelem</h2>
+            <p>A szálloda elkötelezett az Ön személyes adatainak védelme iránt, és az adatvédelmi gyakorlatában szigorúan betartja a helyi adatvédelmi törvényeket. Személyes információit bizalmasan kezeljük, és kizárólag a szolgáltatásaink nyújtása érdekében használjuk fel, harmadik felekkel való megosztás nélkül.</p>
+        </section>
+        <section id="cookie">
+            <h2>Sütik</h2>
+            <p>A hotel weboldala sütiket használ azáltal, hogy információt gyűjt a felhasználók böngészési szokásairól és preferenciáiról, hogy személyre szabottabb élményt nyújthasson. Ezek a sütik lehetővé teszik számunkra, hogy megjegyezzük, hogy mely oldalakat tekintette meg a felhasználó, és milyen beállításokat választott. Ezenkívül harmadik fél sütiket is használhatunk, például a hirdetések célzása vagy a weboldalunk teljesítményének elemzése érdekében. Ezek a sütik nem gyűjtenek személyes adatokat.</p>
+        </section>
+    </main>
+    <footer>
+        <p>© 2024 Minden jog fenntartva.</p>
+    </footer>
+</body>
+</html>
---
+++ view/termsandcondition.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>HappyHotel</title>
+    <link rel="icon" href="../img/logo.jpg" type="image/jpg">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">  
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        <!-- Header Start -->
+        <div class="container-fluid bg-dark px-0">
+            <div class="row gx-0">
+                <div class="col-lg-3 bg-dark d-none d-lg-block">
+                    <a href="index.php" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                        <h1 class="m-0 text-primary text-uppercase">HappyHotel</h1>
+                    </a>
+                </div>
+                <div class="col-lg-9">
+                    <div class="row gx-0 bg-white d-none d-lg-flex">
+                        <div class="col-lg-7 px-5 text-start">
+                            <div class="h-100 d-inline-flex align-items-center py-2 me-4">
+                                <i class="fa fa-envelope text-primary me-2"></i>
+                                <p class="mb-0">info@example.com</p>
+                            </div>
+                            <div class="h-100 d-inline-flex align-items-center py-2">
+                                <i class="fa fa-phone-alt text-primary me-2"></i>
+                                <p class="mb-0">+012 345 6789</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 px-5 text-end">
+                            <div class="d-inline-flex align-items-center py-2">
+                                <a class="me-3" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                <a class="me-3" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                                <a class="me-3" href="https://hu.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                                <a class="me-3" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
+                        <a href="index.php" class="navbar-brand d-block d-lg-none">
+                            <h1 class="m-0 text-primary text-uppercase">HappyHotel</h1>
+                        </a>
+                        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                            <div class="navbar-nav mr-auto py-0">
+                                <a href="index.php" class="nav-item nav-link active">Home</a>
+                                <a href="about.php" class="nav-item nav-link">About</a>
+                                <a href="service.php" class="nav-item nav-link">Services</a>
+                                <a href="room.php" class="nav-item nav-link">Rooms</a>
+                                <a href="kedvezmenyeink.php" class="nav-item nav-link">Kedvezményeink</a>
+
+                                <a href="booking.php" class="nav-item nav-link">Booking</a>
+                                <a href="signup.php" class="nav-item nav-link">Registration/Login</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+
+        <!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms and Condition</title>
+</head>
+<body>
+    <header>
+        <h1>Üdvözöljük a HappyHotel weboldalán! Kérjük, olvassa el figyelmesen az alábbi felhasználási feltételeket, mielőtt használná weboldalunkat.</h1>
+    </header>
+    <main>
+        <section id="terms">
+        <h2>Általános információk</h2>
+A HappyHotel weboldalának használata a felhasználók és a hotel közötti kapcsolat része. Az oldalon történő navigáció és szolgáltatásaink igénybevétele a jelen feltételek elfogadását jelenti.
+A weboldal felhasználói kötelesek betartani az összes érvényes törvényt és rendeletet, amely vonatkozik a weboldal használatára, a szálloda szolgáltatásainak igénybevételére, valamint az itt közzétett információkra.
+<h2>Felhasználói felelősség</h2>
+<p>A felhasználók felelősséget vállalnak minden olyan tevékenységért, amelyet a HappyHotel weboldalán keresztül végeznek. Ez magában foglalja az összes regisztrációs adat valóságtartalmát és aktualitását.
+A felhasználók kötelesek biztosítani, hogy a weboldalt nem használják jogellenes célokra vagy más felhasználók számítógépeinek károsítására.</p>
+<h2>Szállásfoglalás és fizetés</h2>
+<p>A szállásfoglalás és fizetés szabályai az egyes foglalásoknál eltérőek lehetnek. Kérjük, olvassa el az egyes ajánlatok részletes leírását és feltételeit.</p>
+A fizetési módok elfogadásáról és a foglalások megerősítéséről további információkat kaphat ügyfélszolgálatunkon keresztül.
+<h2>Lemondási politika</h2>
+<p>A lemondási feltételek és díjak szintén változóak lehetnek. Kérjük, olvassa el a foglalásokhoz kapcsolódó részletes információkat a lemondási feltételekről.</p>
+A lemondási politikáról további információkat kaphat ügyfélszolgálatunkon keresztül.
+<h2>Adatvédelem</h2>
+<p>A HappyHotel elkötelezett az Ön személyes adatainak védelme iránt. Adatvédelmi gyakorlatunkról bővebb információt talál az adatvédelmi irányelveinkben.</p>
+<h2>Szerzői jogok</h2>
+<p>Minden tartalom (szöveg, képek, grafikák stb.) a HappyHotel tulajdonát képezi. A tartalmakat nem szabad engedély nélkül másolni, reprodukálni vagy terjeszteni.</p>
+<h2>Módosítások jogát fenntartjuk</h2>
+<p>A HappyHotel fenntartja a jogot, hogy bármikor módosítsa vagy frissítse ezt a Felhasználási feltételeket. Kérjük, rendszeresen ellenőrizze az oldalt a legfrissebb információkért.
+Kérjük, vegye figyelembe, hogy ezek a Felhasználási feltételek csak tájékoztató jellegűek, és nem helyettesítik a konkrét foglalásokra vonatkozó szerződéseket vagy megállapodásokat. Amennyiben kérdései merülnének fel, kérjük, lépjen kapcsolatba ügyfélszolgálatunkkal.</p>
+        </section>
+        
+    </main>
+    <footer>
+        <p>© 2024 Minden jog fenntartva.</p>
+    </footer>
+</body>
+</html>
+
+        <!-- Footer Start -->
+        <div class="container-fluid bg-dark text-light footer wow fadeIn" data-wow-delay="0.1s">
+            <div class="container pb-5">
+                <div class="row g-5">
+                    <div class="col-lg-5 col-md-12">
+                        <div class="row gy-5 g-4">
+                            <div class="col-md-6">
+                                <h6 class="section-title text-start text-primary text-uppercase mb-4">Company</h6>
+                                <a class="btn btn-link" href="about.php">About Us</a>
+                                <a class="btn btn-link" href="privacypolicy.php">Privacy Policy</a>
+                                <a class="btn btn-link" href="termsandconditons.php">Terms & Condition</a>
+                                <a class="btn btn-link" href="">Support</a>
+                            </div>
+                            <div class="col-md-6">
+                                <h6 class="section-title text-start text-primary text-uppercase mb-4">Services</h6>
+                                <a class="btn btn-link" href="">Food & Restaurant</a>
+                                <a class="btn btn-link" href="">Event & Party</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-end">
+                            <div class="footer-menu">
+                                <a href="index.php">Home</a>
+                                <a href="">Cookies</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../lib/wow/wow.min.js"></script>
+    <script src="../lib/easing/easing.min.js"></script>
+    <script src="../lib/waypoints/waypoints.min.js"></script>
+    <script src="../lib/counterup/counterup.min.js"></script>
+    <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="../lib/tempusdominus/js/moment.min.js"></script>
+    <script src="../lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="../lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="../js/main.js"></script>
+</body>
+
+</html>
