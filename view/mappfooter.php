e7d0e9a70faf0468d55191c38a53f05c4a349ef0
Zoltangl <zoltangulcsik@gmail.com>
2024-03-21 12:49:46 +0100

    elérési útvonal
    Elérési utvonal javítása hogy mindenhonnan el lehessen érni a fejlécben szereplő oldalakat

---
+++ view/mappfooter.php
@@ -0,0 +1,32 @@
+<div class="container-fluid bg-dark text-light footer wow fadeIn" data-wow-delay="0.1s">
+            <div class="container pb-5">
+                <div class="row g-5">
+                    <div class="col-lg-5 col-md-12">
+                        <div class="row gy-5 g-4">
+                            <div class="col-md-6">
+                                <h6 class="section-title text-start text-primary text-uppercase mb-4">Company</h6>
+                                <a class="btn btn-link" href="../about.php">About Us</a>
+                                <a class="btn btn-link" href="../privacypolicy.php">Privacy Policy</a>
+                                <a class="btn btn-link" href="../termsandcondition.php">Terms & Condition</a>
+                        </div>
+                        <div class="col-md-6">
+                            <h6 class="section-title text-start text-primary text-uppercase mb-4">Services</h6>
+                            <a class="btn btn-link" href="services/foodres.php">Food & Restaurant</a>
+                            <a class="btn btn-link" href="services/sport.php">Sport & Gym</a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-end">
+                            <div class="footer-menu">
+                                <a href="index.php">Home</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
---
+++ view/mappheader.php
@@ -0,0 +1,50 @@
+<div class="container-fluid bg-dark px-0">
+    <div class="row gx-0">
+        <div class="col-lg-3 bg-dark d-none d-lg-block">
+            <a href="index.php" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                <h1 class="m-0 text-primary text-uppercase">HappyHotel</h1>
+            </a>
+        </div>
+        <div class="col-lg-9">
+            <div class="row gx-0 bg-white d-none d-lg-flex">
+                <div class="col-lg-7 px-5 text-start">
+                    <div class="h-100 d-inline-flex align-items-center py-2 me-4">
+                        <i class="fa fa-envelope text-primary me-2"></i>
+                        <p class="mb-0">HappyHotel@gamil.com</p>
+                    </div>
+                    <div class="h-100 d-inline-flex align-items-center py-2">
+                        <i class="fa fa-phone-alt text-primary me-2"></i>
+                        <p class="mb-0">+36 20 523 4270</p>
+                    </div>
+                </div>
+                <div class="col-lg-5 px-5 text-end">
+                    <div class="d-inline-flex align-items-center py-2">
+                        <a class="me-3" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                        <a class="me-3" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                        <a class="me-3" href="https://hu.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="me-3" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                        <a target="_blank" class="me-3" href="https://youtu.be/dQw4w9WgXcQ?si=o20Z4PnRRefLHeiO"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
+                <a href="index.php" class="navbar-brand d-block d-lg-none">
+                    <h1 class="m-0 text-primary text-uppercase">HappyHotel</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav mr-auto py-0">
+                        <a href="../index.php" class="nav-item nav-link">Home</a>
+                        <a href="../about.php" class="nav-item nav-link">About</a>
+                        <a href="../service.php" class="nav-item nav-link">Services</a>
+                        <a href="../room.php" class="nav-item nav-link">Rooms</a>
+                        <a href="../booking.php" class="nav-item nav-link">Booking</a>
+                        <?php echo $profile_display; ?>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
