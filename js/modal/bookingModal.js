5248ea6daaa21319c6dedbc513904b6349cc6411
Zoltangl <zoltangulcsik@gmail.com>
2024-04-12 01:46:58 +0200

    delete bookingmodal
    torolve a javascript, nem hasznalom inkabb

--- js/modal/bookingModal.js
+++
@@ -1,7 +0,0 @@
-
-// Modal ablak bezárása
-var closeButton = document.querySelector('.modal-header .btn-close');
-closeButton.addEventListener('click', function () {
-    var modal = document.getElementById("exampleModal");
-    modal.style.display = "none"; 
-});
